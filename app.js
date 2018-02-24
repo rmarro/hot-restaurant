0be4fa1ec4d7701157023e73bc1a2dd19d5e2ddc
rmarro <rmarro47@gmail.com>
2018-02-24 13:27:36 -0600

    created reservation constructor, and logic for adding new reservation to tables or waitlist arrays

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var Reservation = require("./reservation.js")
+
 var app = express();
 var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,3 +13,19 @@ app.use(bodyParser.json());
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
+
+var tablesArray = [];
+var waitlistArray = [];
+
+function makeReservation() {
+    // whatever user enters into form will be the parameters for new Reservation
+    var newReservation = new Reservation( // add form input here 
+    );
+    // add new reservation object to tables, or waitlist if tables are full
+    if (tablesArray.length < 5) {
+        tablesArray.push(newReservation);
+    } else {
+        waitlistArray.push(newReservation);
+    }
+};
